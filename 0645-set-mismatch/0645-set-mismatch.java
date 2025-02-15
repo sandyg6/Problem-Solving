584bc546c917118903d182121196b397cb3f2ffe
Sandhiya <118624179+sandyg6@users.noreply.github.com>
2025-02-15 20:17:49 +0530

    Time: 3 ms (82.37%), Space: 45.5 MB (81.21%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -3,21 +3,20 @@ class Solution {
         int n = nums.length;
         int[] result = new int[2];
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int num: nums){
-            hm.put(num, hm.getOrDefault(num, 0) + 1);
-        }
-
-        for(int num: hm.keySet()){
-            if(hm.get(num) == 2){
-                result[0] = num;
-                break;
+        //Identify the duplicated number
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] < 0) {
+                result[0] = num;  // Duplicated number
+            } else {
+                nums[num - 1] = -nums[num - 1];  // Negate the number at index (num - 1)
             }
         }
 
-        for(int i = 1; i <= n; i++){
-            if(!hm.containsKey(i)){
-                result[1] = i;
+        //Find the missing number
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result[1] = i + 1;  // The missing number is i + 1
                 break;
             }
         }
