f749021ec0145fb7ea7b3b28be89e4ff104df390
Sandhiya <118624179+sandyg6@users.noreply.github.com>
2025-04-09 15:04:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.92%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,30 +1,19 @@
 class Solution {
-    // private void rev(int[] arr){
-    //     int left = 0;
-    //     int right = arr.length - 1;
-    //     while(left < right){
-    //         int temp = arr[left];
-    //         arr[left] = arr[right];
-    //         arr[right] = temp;
-    //     }
-    // }
-    public void rotate(int[][] matrix) {
-        for(int i = 0;i < matrix.length; i++){
-            for(int j = i+1; j < matrix[i].length ;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        for(int i = 0; i <n; i++){
+            for(int j = i; j < n; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
-        for(int i = 0; i < matrix.length; i++){
-            int[] arr = matrix[i];
-            int left = 0;
-            int right = arr.length - 1;
-            while(left < right){
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;right--;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-j-1];
+                mat[i][n-j-1] = temp;
             }
         }
     }
